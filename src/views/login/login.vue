1fcc7586034ea73e84fccb40fa827d3b3c8d93e9
WSXGP <815905249@qq.com>
2020-02-07 15:52:09 +0800

    登录模块 - 整体布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>登录页面</div>
+  <!-- 整体容器 -->
+  <div class="login-container">
+    <!-- 左侧盒子 -->
+    <div class="left-box"></div>
+    <img src="../../assets/login_banner_ele.png" alt />
+  </div>
 </template>
 
 <script>
 export default {
-    // name:'login'
-}
+  name: "login"
+};
 </script>
 
 <style>
-
+.login-container {
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.login-container .left-box {
+  width: 478px;
+  height: 550px;
+  background-color: #F5F5F5;
+}
 </style>
