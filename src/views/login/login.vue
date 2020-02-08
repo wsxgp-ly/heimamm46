4484af71a29a88add3b8fa79a0cb9d432121ccec
WSXGP <815905249@qq.com>
2020-02-08 11:46:31 +0800

    登录模块-全部完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -14,8 +14,8 @@
         <!-- 表单 -->
         <el-form ref="form" :model="loginForm" label-width="42px">
           <!-- 手机号 -->
-          <el-form-item >
-            <el-input  v-model="loginForm.user" placeholder="请输入用户名" prefix-icon="el-icon-user"></el-input>
+          <el-form-item>
+            <el-input v-model="loginForm.user" placeholder="请输入用户名" prefix-icon="el-icon-user"></el-input>
           </el-form-item>
           <!-- 密码 -->
           <el-form-item>
@@ -30,23 +30,27 @@
           <el-form-item>
             <el-row>
               <el-col :span="17">
-              <el-input v-model="loginForm.loginCode" placeholder="请输入验证码" prefix-icon="el-icon-key"></el-input>
-            </el-col>
-            <el-col :span="7">
-              <img class="login-code" src="../../assets/login_captcha.png" alt="">
-            </el-col>
+                <el-input
+                  v-model="loginForm.loginCode"
+                  placeholder="请输入验证码"
+                  prefix-icon="el-icon-key"
+                ></el-input>
+              </el-col>
+              <el-col :span="7">
+                <img class="login-code" src="../../assets/login_captcha.png" alt />
+              </el-col>
             </el-row>
           </el-form-item>
           <!-- 用户协议 -->
           <el-form-item>
-              <el-checkbox v-model="loginForm.ischeckout">
-                我已阅读并同意
-                <el-link type="primary">用户协议</el-link>和
-                <el-link type="primary">隐私条款</el-link>
-              </el-checkbox>
+            <el-checkbox v-model="loginForm.ischeckout">
+              我已阅读并同意
+              <el-link type="primary">用户协议</el-link>和
+              <el-link type="primary">隐私条款</el-link>
+            </el-checkbox>
           </el-form-item>
           <el-form-item>
-            <el-button class="mybut" type="primary" >登录</el-button>
+            <el-button class="mybut" type="primary">登录</el-button>
             <el-button class="mybut" type="primary">注册</el-button>
           </el-form-item>
         </el-form>
@@ -70,11 +74,10 @@ export default {
         // 验证码
         loginCode: "",
         // 勾选框
-        ischeckout: false,
+        ischeckout: false
       }
     };
-  },
-  
+  }
 };
 </script>
 
@@ -90,7 +93,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-around;
-// 左侧盒子
+  // 左侧盒子
   .left-box {
     width: 478px;
     height: 550px;
@@ -124,29 +127,31 @@ export default {
     .content {
       width: 100%;
       margin-top: 29px;
-      
-          .imgs{
-              width: 100px;
-              height: 40px;
-          }
-        .mybut {
-          margin-left: 0;
-          width: 100%;
-          margin-bottom: 25px;
-        }
+
+      .imgs {
+        width: 100px;
+        height: 40px;
       }
+      // 按钮
+      .mybut {
+        margin-left: 0;
+        width: 100%;
+        margin-bottom: 25px;
+      }
+    }
+    // 登录验证码
+    .login-code {
+      width: 100%;
+      height: 41px;
+    }
   }
-  // 登录验证码
-  .login-code {
-    width: 100%;
-    height: 41px;
-  }
+
   .el-checkbox {
     display: flex;
+    align-items: center;
     .el-checkbox__label {
       display: flex;
     }
   }
-
 }
 </style>
